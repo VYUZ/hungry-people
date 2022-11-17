a40bc7f855b2f1922760619a994c6e11d2a38658
Vladislav <valdiss1978@gmail.com>
2022-11-17 11:46:29 +0700

    adaptive 2

--- src/js/main.js
+++ src/js/main.js
@@ -18,6 +18,29 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 });
+
+/////////////////////////////////////////////////////////////
+
+/* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
+function isWebp() {
+  //Проверка поддержки webp
+  function testWebP(callback) {
+    // console.log("supported");
+    let webP = new Image();
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+    };
+    webP.src =
+      "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  }
+  //Добавление класса _webp или _no-webp для HTML
+  testWebP(function (support) {
+    let className = support === true ? "webp" : "no-webp";
+    document.documentElement.classList.add(className);
+  });
+}
+isWebp();
+
 /////////////////////////////////////////////////////////////
 
 //======== меню "menu" (DELICIOUS MENU) ==========
@@ -241,26 +264,6 @@ Tu.tScroll({
 });
 
 ///////////////////////////////////////////
-
-/* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
-function isWebp() {
-  //Проверка поддержки webp
-  function testWebP(callback) {
-    // console.log("supported");
-    let webP = new Image();
-    webP.onload = webP.onerror = function () {
-      callback(webP.height == 2);
-    };
-    webP.src =
-      "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-  }
-  //Добавление класса _webp или _no-webp для HTML
-  testWebP(function (support) {
-    let className = support === true ? "webp" : "no-webp";
-    document.documentElement.classList.add(className);
-  });
-}
-isWebp();
 
 ///////////////////////////////////////////
 
